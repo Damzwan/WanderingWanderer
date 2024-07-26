4ffe4d58643e13f94e22fe5112aeddb4524f08b3
Damian <damian.vlaicu@gmail.com>
2024-07-26 12:08:57 +0800

    changed astrojs config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://damzwan.github.io',
-	base: "wanderingwanderer",
+	site: 'https://damzwan.github.io/wanderingwanderer',
 	integrations: [mdx(), sitemap()],
 });
